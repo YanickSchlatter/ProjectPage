8da3a4b675a48788505ec64d70fa5d0c81f3101f
YanickSchlatter <37781711+YanickSchlatter@users.noreply.github.com>
2018-10-23 22:43:04 +0200

    adding content

---
+++ ProjectPage_YanickSchlatter/ProjectPage_YanickSchlatter/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Allgemeine Informationen über eine Assembly werden über die folgenden 
+' Attribute gesteuert. Ändern Sie diese Attributwerte, um die
+' Assemblyinformationen zu ändern.
+
+' Werte der Assemblyattribute überprüfen
+<Assembly: AssemblyTitle("ProjectPage_YanickSchlatter")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Hewlett-Packard")>
+<Assembly: AssemblyProduct("ProjectPage_YanickSchlatter")>
+<Assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2018")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM bereitgestellt wird.
+<Assembly: Guid("8b2de02b-441e-477d-8317-6c2dac68efe2")>
+
+' Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
+' übernehmen, indem Sie "*" wie folgt verwenden:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ ProjectPage_YanickSchlatter/ProjectPage_YanickSchlatter/Scripts/js/my.js
@@ -0,0 +1,21 @@
+﻿$(window).on('ready', function () {
+
+
+
+});
+$(window).on('load', function () {
+    $(".blackBorder").css({ "border": "solid 2px", "border-color": "black", "margin": "5% auto", "width": "80%", "padding-left": "20px" });
+    $(".jQueryLogo").css({ "width": "20%" });
+    $(".jQuerytext").css({ "color": "#0769AA" });
+    $("#jQueryMessage").css({ "font-weight": "bold" });
+
+    $(".jQueryLogo").click(function () {
+        $(this).css({ "border": "solid 10px", "border-color": "blue", "width": "80%"});
+        $("#jQueryMessage").text("Doppelklicken um zurückzukehren");
+    });
+
+    $(".jQueryLogo").dblclick(function () {
+        $(this).css({ "border": "solid 0px", "border-color": "transparent", "width": "20%"});
+        $("#jQueryMessage").text("Klicke auf das Logo");
+    });
+});
